2bbbb13b14987c0444ee7e631c396cb478980b2c
Thales <meier.thales@gmail.com>
2023-11-20 23:28:00 -0300

    New models

--- pegaso/db/models.py
+++ pegaso/db/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Project(models.Model):
     type = models.CharField(max_length=5)
-    number = models.IntegerField(max_length=4)
+    number = models.IntegerField()
 
     # Performance data
     cruise_speed = models.IntegerField()
@@ -13,12 +13,28 @@ class Project(models.Model):
     max_ldg_weight = models.IntegerField()  # Landing
     burn_rate = models.IntegerField()  # TODO: Define standard unit
     max_passengers = models.IntegerField()
-    max_flight_time = models.FloatField()
+    max_flight_time = models.TimeField()
+
+    def __str__(self):
+        return "{} {}".format(self.type, self.number)
 
 
 class Qualification(models.Model):
     name = models.CharField(max_length=2)
+    description = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
-class User(AbstractUser):
-    pass
+class Airport(models.Model):
+    icao = models.CharField(max_length=4)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    has_fueling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.icao
